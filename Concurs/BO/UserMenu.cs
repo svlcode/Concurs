b2f303c7006065bf3fb0e03bfdb13f17a47a5182
atharisra <t.raluca.elena@gmail.com>
2017-03-25 12:12:33 +0200

    moved files to bo

--- Concurs/BO/UserMenu.cs
+++ Concurs/BO/UserMenu.cs
@@ -1,7 +1,27 @@
-﻿namespace Concurs.BO
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concurs
 {
     public class UserMenu
     {
-         
+        public string UserId { get; set; }
+        public List<DayMenu> DayMenu { get; set; }
+
+
+        public UserMenu()
+        {
+            DayMenu = new List<DayMenu>();
+        }
+    }
+
+    public class DayMenu
+    {
+        public DateTime Day { get; set; }
+        public string F1 { get; set; }
+        public string F2 { get; set; }
     }
 }
