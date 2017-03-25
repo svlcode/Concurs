2c76064aa2312d338b9fa9ee4da5b0ebcdcd779f
atharisra <t.raluca.elena@gmail.com>
2017-03-25 12:09:16 +0200

    added usermenu and predict file

---
+++ Concurs/Predict.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concurs
+{
+    public class Predict
+    {
+        UserMenu _userMenu;
+
+        public Predict(UserMenu usermenu)
+        {
+            _userMenu = usermenu;
+        }
+
+        public UserMenu Generate()
+        {
+            var result = new UserMenu();
+
+            SetData();
+            result = RunPrediction();
+
+            return result;
+        }
+
+        public void SetData()
+        {
+
+        }
+
+        UserMenu RunPrediction()
+        {
+            return new UserMenu();
+        }
+}
---
+++ Concurs/UserMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concurs
+{
+    public class UserMenu
+    {
+        public string UserId { get; set; }
+        public List<DayMenu> DayMenu { get; set; }
+
+
+        public UserMenu()
+        {
+            DayMenu = new List<DayMenu>();
+        }
+    }
+
+    public class DayMenu
+    {
+        public DateTime Day { get; set; }
+        public string F1 { get; set; }
+        public string F2 { get; set; }
+    }
+}
