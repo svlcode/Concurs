ad91dbf2bc634d0b4327f687aca94cd17353f15e
svlcode <vio.slaniceanu@gmail.com>
2017-03-25 11:21:19 +0200

    Remove 4.6.1 constrains.

---
+++ Concurs/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Concurs
+{
+    public class User
+    {
+        public string UID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
