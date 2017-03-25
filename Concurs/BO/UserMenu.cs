28f5f1192fdd65e307c89361330d63cf6ccc27ea
atharisra <t.raluca.elena@gmail.com>
2017-03-25 12:13:12 +0200

    Revert "moved files to bo"
    This reverts commit b2f303c7006065bf3fb0e03bfdb13f17a47a5182.

--- Concurs/BO/UserMenu.cs
+++ Concurs/BO/UserMenu.cs
@@ -1,27 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Concurs
+﻿namespace Concurs.BO
 {
     public class UserMenu
     {
-        public string UserId { get; set; }
-        public List<DayMenu> DayMenu { get; set; }
-
-
-        public UserMenu()
-        {
-            DayMenu = new List<DayMenu>();
-        }
-    }
-
-    public class DayMenu
-    {
-        public DateTime Day { get; set; }
-        public string F1 { get; set; }
-        public string F2 { get; set; }
+         
     }
 }
